15887729fe2f1c2ca7846934f7a0564205c7a688
Ahmad Mash <wolfmashagbah@hotmail.com>
2023-09-27 17:10:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  let result;
+  if (distance <= 400) {
+    result = 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    result = 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (distance >= 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  let result;
+  result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+console.log('NYC')
+console.log('Pittsburgh')
+
+function switchOnCharmFromTip(tip){
+  let result
+switch (tip) {
+  case 'generous': 
+    result = 'Thank you so much.'
+  break;
+  case 'not as generous': 
+    result = 'Thank you.'
+  break;
+  default: 
+    result = 'Bye.'
+}
+return result
 }
